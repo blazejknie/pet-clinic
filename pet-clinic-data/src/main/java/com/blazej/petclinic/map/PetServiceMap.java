5442f035a2635c5bbe2d4af48475f5f727d554f8
Błażej <blazejknie@gmail.com>
2021-11-21 22:09:46 +0100

    Impl of map services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/blazej/petclinic/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.blazej.petclinic.map;
+
+import com.blazej.petclinic.model.Pet;
+import com.blazej.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(),pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
