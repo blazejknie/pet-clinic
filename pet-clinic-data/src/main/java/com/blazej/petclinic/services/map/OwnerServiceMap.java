9577237584f0ece342fdbc60281b3ab8604d0d78
Błażej <blazejknie@gmail.com>
2021-11-27 23:14:20 +0100

    Added Pets and contact info to Owners at Bootstrap. Closes #33

--- pet-clinic-data/src/main/java/com/blazej/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/blazej/petclinic/services/map/OwnerServiceMap.java
@@ -1,12 +1,24 @@
 package com.blazej.petclinic.services.map;
 
 import com.blazej.petclinic.model.Owner;
+import com.blazej.petclinic.model.Pet;
 import com.blazej.petclinic.services.OwnerService;
+import com.blazej.petclinic.services.PetService;
+import com.blazej.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -19,6 +31,26 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
+        if (owner != null) {
+            if (owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else {
+                        throw new RuntimeException("Pet must have Type");
+                    }
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+
+        }else {
+            return null;
+        }
         return super.save(owner);
     }
 
