f3b66d2ba92f6da42e66ff9bf9d38f32e98556a4
Błażej <blazejknie@gmail.com>
2021-11-28 12:26:03 +0100

    Added Specialities to Vets on startup. Closes 34

--- pet-clinic-data/src/main/java/com/blazej/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/blazej/petclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.blazej.petclinic.services.map;
 
+import com.blazej.petclinic.model.Speciality;
 import com.blazej.petclinic.model.Vet;
+import com.blazej.petclinic.services.SpecialityService;
 import com.blazej.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +29,19 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (vet == null) {
+            return null;
+        }
+
+        vet.getSpecialities().forEach(speciality -> {
+            if (speciality != null) {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            }
+        });
+
         return super.save(vet);
     }
 
