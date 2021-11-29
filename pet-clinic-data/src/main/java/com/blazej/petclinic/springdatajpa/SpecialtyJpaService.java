c45e1b5b2faaa1085a56bef60a42da5f9c31f27e
Błażej <blazejknie@gmail.com>
2021-11-29 22:23:20 +0100

    Added Spring Data Jpa Service for Vet Specialties . Closes #44

---
+++ pet-clinic-data/src/main/java/com/blazej/petclinic/springdatajpa/SpecialtyJpaService.java
@@ -0,0 +1,48 @@
+package com.blazej.petclinic.springdatajpa;
+
+import com.blazej.petclinic.model.Specialty;
+import com.blazej.petclinic.repository.SpecialtyRepository;
+import com.blazej.petclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtyJpaService implements SpecialityService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
