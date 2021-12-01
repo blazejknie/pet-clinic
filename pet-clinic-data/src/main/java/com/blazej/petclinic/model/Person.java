3aadf0a428e6672715d36f2a479fd336d8321ae3
Błażej <blazejknie@gmail.com>
2021-12-01 22:44:35 +0100

    Added project lombok and implement to classes

--- pet-clinic-data/src/main/java/com/blazej/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/blazej/petclinic/model/Person.java
@@ -1,7 +1,15 @@
 package com.blazej.petclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
     @Column(name = "first_name")
@@ -9,36 +17,11 @@ public class Person extends BaseEntity {
     @Column(name = "last_name")
     private String lastName;
 
-
-    public Person(String firstName, String lastName) {
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public Person() {
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
--- pet-clinic-data/src/main/java/com/blazej/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/blazej/petclinic/model/PetType.java
@@ -1,34 +1,20 @@
 package com.blazej.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "type")
 public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
 
-    public PetType(String name) {
-        this.name = name;
-    }
-
-    public PetType() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "PetType{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
